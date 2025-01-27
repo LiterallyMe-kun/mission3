2b9385db8499787b974252d9753455fdf3efda8a
LiterallyMe-kun <sknoah2002@gmail.com>
2025-01-27 15:26:32 -0700

    yes.

--- FoodItem.cs
+++ FoodItem.cs
@@ -3,11 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using mission3;
 
 namespace mission3
 {
-    internal class FoodItem
+     public class FoodItem
     {
+        List<string> food = new List<string>();
+        public void FoodLibrary(int theNumber)
+        {
+            switch (theNumber)
+            {
+                case 1:
+                    string newFood;
+                    Console.WriteLine("What food are you giving?");
+                    newFood = Console.ReadLine();
+                    Console.WriteLine("What category? eg. dairy, grain");
+                    newFood = newFood + ", " + Console.ReadLine();
+                    Console.WriteLine("How much?");
+                    newFood = newFood + ", " + Console.ReadLine();
+                    Console.WriteLine("Expiration date?");
+                    newFood += ", EXP DATE: " + Console.ReadLine();
+                    food.Add(newFood);
+                    Console.WriteLine("Food Added");
+                    break;
+                case 2:
+                    
+                    int daIndex = 0;
+                    Console.WriteLine("Delete which item? First line printed = 1");
+                    foreach (string food in food) 
+                    {
+                        Console.WriteLine(food); 
+                    }
+                bruh:
+                    daIndex = int.Parse(Console.ReadLine()) - 1;
+                    if (daIndex > food.Count || daIndex < 0)
+                    {
+                        Console.WriteLine("Please enter only a number corresponding to the number of printed lines.");
+                        goto bruh;
+                    }
+                    else
+                    {
+                        food.RemoveAt(daIndex);
+                        Console.WriteLine("Item Removed");
+                        break;
+                    }
+                    
+                case 3:
+                    foreach (string food in food) 
+                    {Console.WriteLine(food); }
+                    break;
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    Environment.Exit(0);
+                    break;
 
+
+
+            };
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System.Runtime.InteropServices.Marshalling;
+using mission3;
+
+internal class Program
 {
+   
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        FoodItem fi = new FoodItem();
+
+        bool loopCheck = true;
+        int optCheck = 0;
+
+        while (loopCheck == true)
+        {
+            Console.WriteLine("Press 1 to add a food item.");
+            Console.WriteLine("Press 2 to delete a food item.");
+            Console.WriteLine("Press 3 to print all food items.");
+            Console.WriteLine("Press 4 to exit the program.");
+            optCheck = int.Parse(Console.ReadLine());
+
+            if (optCheck != 1 || optCheck != 2 || optCheck != 3 || optCheck != 4)
+            {
+                Console.WriteLine("Please only click 1, 2, 3, or 4");
+            }
+            else 
+            {fi.FoodLibrary(optCheck); }
+        }
     }
 }
