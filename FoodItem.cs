b70b8375c73e1d8595f70d4d60f952ded71cecd6
LiterallyMe-kun <sknoah2002@gmail.com>
2025-01-28 09:35:56 -0700

    comments

--- FoodItem.cs
+++ FoodItem.cs
@@ -9,47 +9,59 @@ namespace mission3
 {
      public class FoodItem
     {
+        // I don't even remember why I used this construct, but it works
         List<string> food = new List<string>();
         public void FoodLibrary(int theNumber)
         {
             switch (theNumber)
             {
                 case 1:
+                    // concat one big string of data to append to food list because simple
                     string newFood;
                     Console.WriteLine("What food are you giving?");
-                    newFood = Console.ReadLine();
+                    newFood = "" + Console.ReadLine();
+
                     Console.WriteLine("What category? eg. dairy, grain");
                     newFood = newFood + ", " + Console.ReadLine();
+
                     Console.WriteLine("How much?");
                     newFood = newFood + ", " + Console.ReadLine();
+
                     Console.WriteLine("Expiration date?");
                     newFood += ", EXP DATE: " + Console.ReadLine();
+
                     food.Add(newFood);
                     Console.WriteLine("Food Added");
                     break;
                 case 2:
-                    
+
+                    // variable for user input
                     int daIndex = 0;
 
-                    
                     Console.WriteLine("Delete which item? First line printed = 1. If blank, enter 0");
                     foreach (string food in food) 
                     {
                         Console.WriteLine(food); 
                     }
+                // goto label for user ease
                 bruh:
+                    // tryparse if to catch bad data
                     if (int.TryParse(Console.ReadLine(), out daIndex))
                     {
                         daIndex = daIndex - 1;
+                        // empty list failsafe
                         if (daIndex == -1 )
                         {
                             break;
                         }
+                        // invalid index failsafe
                         if (daIndex > food.Count || daIndex < -1)
                         {
                             Console.WriteLine("Please enter only a number corresponding to the number of printed lines.");
+                            //goto for user convenience 
                             goto bruh;
                         }
+                        // successful deletion
                         else
                         {
                             food.RemoveAt(daIndex);
@@ -57,17 +69,23 @@ namespace mission3
                             break;
                         }
                     }
+                    // string data failsafe
                     else
                     {
                         Console.WriteLine("Enter a valid integer.");
+                        // I use goto because I'm a G and don't give a hoot about good practice
                         goto bruh;
                     }
                     
                 case 3:
+                    //simple foreach print
                     foreach (string food in food) 
-                    {Console.WriteLine(food); }
+                    {
+                        Console.WriteLine(food);
+                    }
                     break;
                 case 4:
+                    // don't even change loopCheck, lol, just exit the program.
                     Console.WriteLine("Goodbye!");
                     Environment.Exit(0);
                     break;
--- Program.cs
+++ Program.cs
@@ -3,23 +3,23 @@ using mission3;
 
 internal class Program
 {
-   
-
     private static void Main(string[] args)
     {
+        // call the foodItem class
         FoodItem fi = new FoodItem();
 
         bool loopCheck = true;
         int optCheck = 0;
-        int cringeDude = 0;
         
-
+        // while loop because it works
         while (loopCheck == true)
         {
             Console.WriteLine("Press 1 to add a food item.");
             Console.WriteLine("Press 2 to delete a food item.");
             Console.WriteLine("Press 3 to print all food items.");
             Console.WriteLine("Press 4 to exit the program.");
+
+            // tryparse if to catch bad data 
             if (int.TryParse(Console.ReadLine(), out optCheck))
             {
                 if (optCheck > 4 || optCheck < 1)
